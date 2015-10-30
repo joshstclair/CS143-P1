70705ac29c3b70b73462795545bf32b187c130ed
Dong Joon Kim <dongjoonkim@Dong-Joons-MacBook-Pro.local>
2015-10-30 16:36:50 -0700

    Created the index page and addActorDirector

---
+++ 1C/www/dir.php
@@ -0,0 +1,11 @@
+<html>
+	<head>
+		<title>Navigation</title>
+	</head>
+	<body>
+		Add New Content :
+			<ul>
+				<li><a href="./addActorDirector.php" target="main">Add Actor/Director</a></li>
+			</ul>
+	</body>
+</html>
---
+++ 1C/www/index.php
@@ -0,0 +1,9 @@
+<html>
+<head>
+	<title>CS 143 Project 1C</title>
+</head>
+<FRAMESET COLS="25%,75%" FRAMEBORDER="0" BORDER="0">
+<FRAME NAME="dir" SRC="dir.php">
+<FRAME NAME="main" SRC="addActorDirector.php">
+</FRAMESET>
+</html>
